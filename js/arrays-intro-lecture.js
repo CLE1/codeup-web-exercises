697064d00c142de9de74cb870fb459080597afe3
Christopher <espinoza.christopher117@gmail.com>
2021-04-26 11:29:45 -0500

    1129

---
+++ js/arrays-intro-lecture.js
@@ -0,0 +1,77 @@
+(function() {
+
+    "use strict";
+
+    //number - 5
+    //string - "dog"
+    //boolean - false/true
+    //null - the absence of
+    // undefined - data should exist, but doesnt
+    //object ->
+
+    // []
+
+    // var myArr = ["dog", "cat", "fish"] //<- each comma-delimited value is called
+    //
+    // console.log(Array.isArray(myArr));
+
+    // var myCharacteristics = [ "Chris", "student", "29"];
+
+    // console.log("the length of my array is: " + myCharacteristics.length);
+    // the first element is an array ahs an index of 0
+    // the last element in an array has an index of (length - 1)
+
+    // console.log('hello, I am a ${myCharacteristics[1]} named ${myCharacteristics[0]}');
+
+    // TODO: Make a new array which holds elements based on your own characteristics
+//  -> In a console.log, output a string which describes yourself based on those elements
+//  -> KEEP IN MIND ZERO-INDEXING!
+
+    // var myCharacteristics = [ "Chris", "veteran", "29"];
+    //
+    // console.log(Array.isArray(myCharacteristics));
+
+    // console.log(myCharacteristics[3]); // accessing an index out of the bounds of the array
+    //
+    // myCharacteristics[0] += "Espinoza"; // string are immutable
+    // myCharacteristics[0] = myCharacteristics[0] + "Espinoza";
+    // console.log(myCharacteristics[0]);
+    //
+    // for (let i = 0; i < 10; i++){
+    //
+    //     // do something with ONE array element
+    //
+    //
+    //
+    // }
+
+
+    // var myPets = ["Coda", "Murphy", "Hondo", "Buddy"]
+    //
+    // //length = 4
+    // //maxIndex = 3
+    // for (let i = 0; i < myPets.length; i++){
+    //     console.log(myPets[i]); // 4
+    // }
+
+
+// TODO: MINI-EXERCISE
+//  -> Create an array with at least 5 elements, describing whatever you'd like!
+//  -> Create a loop which will log each element to the console
+//  -> Wrap that loop in a function which has an array parameter
+//  -> Call the function and pass in your array as the argument
+//  -> DID YOU KNOW: You can add different data types to an array??
+//      -> Try adding strings, numbers, or booleans as elements
+
+
+
+
+
+    myPets.forEach(function(pet){
+        // do stuff
+        console.log("My pet's name is:")
+    })
+
+
+
+})();
