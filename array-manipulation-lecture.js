4215a51cbd7765b58406cf0d050579a2c078185f
Christopher <espinoza.christopher117@gmail.com>
2021-04-26 15:26:33 -0500

    reathor commits

---
+++ array-manipulation-lecture.js
@@ -0,0 +1,64 @@
+(function(){
+
+
+    "use strict"
+
+    // //TODO
+    // // -> .length - SUPER IMPORTANT
+    // // -> .forEach() - ALSO IMPORTANT
+    // // -> FOR LOOP - STILL IMPORTANT
+    //
+    // var days = ["Monday", "Tuesday", "Wednesday"];
+    // days.push("Thursday");
+    //
+    // var removedElement = days.pop();
+    //
+    // console.log(days);
+    // console.log("The removed element was: " + removedElement);
+    //
+    //
+
+    // TODO: MINI-EXERCISE
+//  -> var days = ["Monday", "Tuesday", "Wednesday"];
+//  -> Create an array with the remaining days of the week
+//  -> Call .forEach() on that NEW array
+//  -> Inside the inline function you pass into .forEach():
+//      -> add the remainingDay of the week to the old array
+//  -> When the loop completes, console.log() the completed day of the week array
+
+    var days = ["Monday", "Tuesday", "Wednesday"];
+
+    var days = ["Thursday", "Friday", "Saturday", "Sunday"];
+
+
+
+
+
+
+// TODO - MINI EXERCISE
+//  -> Let's reverse engineer our days of the week array
+//  -> Make sure to console.log in order to see your code's behavior!
+//  -> Write a function which takes in your complete days of the week array
+//      -> It will eventually return your reverse-engineered array
+//  -> Using a FOR LOOP (not .forEach())
+//      -> pop() each item off the days array
+//      -> use the returned value from pop() to unshift the element
+//      -> on to the new array
+//  -> HINT: You may find that the loop doesn't behave as expected
+//      -> console.log EVERY possible thing which could change as your loop runs
+//          -> consider how the methods you are using change state of the array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})()
