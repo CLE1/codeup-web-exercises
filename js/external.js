c13316e389182c35e96d418b12f717617d684676
Christopher <espinoza.christopher117@gmail.com>
2021-05-04 10:50:46 -0500

    HTML Elements complete

--- js/external.js
+++ js/external.js
@@ -1,13 +1,17 @@
+
+"use strict";
+
 alert("Welcome to my Website");
+
 
 var userChoice =  prompt("what is your favorite color?");
 
- if (userChoice{
+ if (userChoice){
     console.log("blue")
 }
 console.log("the user chose" + userChoice);
 
- var alert("your favorite color is blue");
+ alert("your favorite color is blue");
 
  if (userInput === "blue");
 
---
+++ js/practice.js
@@ -0,0 +1,18 @@
+"use strict";
+
+
+// Write a function named cube(n) that returns n * n * n
+// Write a function named cubeRoot(n) that returns the cube root of the input
+
+function cube(n){
+    return n * n * n;
+
+}
+
+console.log(cube(2, 2, 2));
+
+
+function cubeRoot(n){
+
+
+}
