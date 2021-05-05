99fb5b6f15cf205a2886fc6c3b8a399e2484d5d7
Christopher <espinoza.christopher117@gmail.com>
2021-05-05 09:56:02 -0500

    updated

---
+++ js/preassessment2-ex.js
@@ -0,0 +1,61 @@
+(function () {
+    "use strict";
+
+    // function getSquare(num){
+    //     if (isNaN(Math.pow(num, 4))){
+    //         return false;
+    //     } else {
+    //         return Math.pow(num, 4);
+    //     }
+    // }
+    //
+    // console.log(getSquare());
+
+
+    // function getReverse(str){
+    //     if (isNaN(Array.reverse(str, "hello"))){
+    //         return false;
+    //     } else {
+    //         return Array.reverse(str, "hello")
+    //     }
+    //
+    // }
+    //
+    // console.log(getReverse());
+
+
+    function multiplySquares(num1, num2){
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
