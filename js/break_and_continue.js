3d49a2558f8447fb75fd6c54bda3c85fbb370d4c
Christopher <espinoza.christopher117@gmail.com>
2021-05-11 01:39:59 -0500

    keepgoing

---
+++ js/break_and_continue.js
@@ -0,0 +1,25 @@
+(function(){
+    "use strict";
+
+    var numberToStopAt = 49;
+
+    for (var i = 1; i < 50; i++) {
+
+        console.log('Here is an odd number: ' + i);
+
+        if (i === 26) {
+            console.log('We have reached the stopping point: break!');
+
+            continue;
+
+            console.log();
+        }
+    }
+
+
+
+
+
+
+})();
+
