a0a1403bb1a43bd62da845e11061ccd275b98a33
Christopher <espinoza.christopher117@gmail.com>
2021-06-01 15:08:19 -0500

    break

---
+++ js/jquery-lecture.js
@@ -0,0 +1,71 @@
+"use strict";
+
+// tapping into objects
+
+
+//syntax - select by tag name
+// $("H1")
+
+//syntax - select by ID
+// $("#change-text")
+// $("#change-text").hide();
+
+//syntax - select by class
+// $(".lead")
+// .hide()
+// $(".purple").hide();
+
+
+// TODO:
+//
+// $("#all-caps").hide();
+//
+// $("span").hide();
+//
+// $("p").show()
+//
+
+
+
+//syntax - selecting children/parent/sibling elements
+// console.log($('ul').children());
+//
+// console.log("add border class");
+// $("#second").addClass("border");
+// console.log("remove vorder class");
+// $("#second").removeClass("border");
+
+// $(".purple-par").setAttribute("class", "border");
+
+// let purPara = $(".purple-par");
+// purPara.attribute("class", "border");
+//
+// $("div").addClass("red");
+
+
+//window.onload
+//javascript
+// do something when the window is loading
+//** its going to wait for images
+
+// window.onload = function() {
+//     console.log(new Date());
+//     console.log("hey window is done loading");
+}
+
+//document.ready
+//jquery
+//wont wait for images
+// $(document).ready(function() {
+//     console.log(new Date());
+//     console.log("Hey the DOM is finished loading")
+// })
+
+// TODO:
+//
+$(".pink").addClass("")
+
+window.onload = function (){
+    $(".pink").hide();
+    console.log("Pink is hidden")
+}
--- js/practice.js
+++ js/practice.js
@@ -4,10 +4,14 @@
 // If either of the arguments are not a numeric string or number, return a string message indicating which argument is not
 // a number/numeric string
 
-function remainder(num1, num2){
-    return num1 % num2;
+// function remainder(num1, num2){
+//     return num1 % num2;
+//
+// }
+//
+// console.log(remainder(9, 23))
 
+
+band.shred() {
+    console.log('RAAAAAAAAAA!!!!');
 }
-
-console.log(remainder(9, 23))
-
