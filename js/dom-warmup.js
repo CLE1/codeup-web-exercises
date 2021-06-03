4eac96adf15900a883bc2c69efea56038d185985
Christopher <espinoza.christopher117@gmail.com>
2021-06-03 11:04:43 -0500

    break

---
+++ js/dom-warmup.js
@@ -0,0 +1,19 @@
+"use strict";
+
+searchThis();
+function searchThis (event) {
+    document.getElementById("inputThis")
+        .addEventListener('click', listener);
+}
+   function listener (e) {
+    e.preventDefault()
+    let inputText = document.getElementById("searchHere").value;
+    let putText = document.getElementById("post-container");
+    putText.innerText = inputText;
+   }
+
+   function styleText (text) {
+    $(text).css("font-size )
+
+   }
+
