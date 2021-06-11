3ab3138ce3f8c40570865238d6cd0ac11f3b5686
Christopher <espinoza.christopher117@gmail.com>
2021-06-10 23:51:28 -0500

    break

---
+++ js/weathermap-playground.js
@@ -0,0 +1,21 @@
+// $.ajax({
+//     url: "http://api.openweathermap.org/data/2.5/weather",
+//     type: "GET",
+//     data: {
+//         APPID: OPEN_WEATHER_TOKEN,
+//         q:     "San Antonio, US"
+//     },
+//     success: function(data){
+//         console.log(data);
+//     }
+// });
+
+
+$.ajax({
+    url: ,
+    type: "GET",
+    data: {
+        APPID: OPEN_WEATHER_APPID,
+        q:     "San Antonio, US"
+    }
+});
