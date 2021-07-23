0d00e6898397fa4b04742c569b23e73d6f1a7050
Christopher <espinoza.christopher117@gmail.com>
2021-07-22 22:37:04 -0500

    break

--- js/JS-warmups.js
+++ js/JS-warmups.js
@@ -1,23 +1,4 @@
-"use strict";
-
-
-// for(var i=1;i<=rows;i++){
-//     console.log(rows());
-//
-// }
-
-//Write a function named secondToLast(arr) that returns the data type of the second to last element of the array
-//parameter. When you call the function, pass in an array of your choosing
-var nombres = [chris, juan, sam, corey, true, 5]
-
-function secondToLast(arr) {
-    return typeof names[names.length - 3]
-}
-
-console.log(secondToLast(nombre));
-
-
-
+const sayHello = (name) => `Hello, ${name}!`
 
 
 
